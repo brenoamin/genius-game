686ed1b47bd50525014bc144271c8e2f00d1c671
Breno Amin <brenomatos125@gmail.com>
2023-06-17 20:14:24 -0300

    feature: add field class and fix the images positions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 from tupy import *
-from utils.buttons_mode import YELLOW_ON, YELLOW_OFF, GREEN_ON, GREEN_OFF, RED_ON, RED_OFF, BLUE_ON, BLUE_OFF
+from utils.buttons_mode import YELLOW_ON, GREEN_ON, RED_ON, BLUE_ON, YELLOW_OFF, GREEN_OFF, RED_OFF, BLUE_OFF, BACKGROUND_SCENE
+
+class Field(Image):
+    def __init__(self):
+        self.file = BACKGROUND_SCENE
+        self.x = 800
+        self.y = 600
+
 
 class Button(Image):
     def __init__(self, file, x, y):
@@ -39,6 +46,9 @@ class Button(Image):
     
 
 if __name__ == '__main__':
-    yellow_button = Button(YELLOW_OFF, 500, 500)
-    red_button = Button(RED_OFF, 200, 100)
+    field = Field()
+    yellow_button = Button(YELLOW_ON, 490, 910)
+    red_button = Button(RED_ON, 1110, 290)
+    blue_button = Button(BLUE_ON, 1110, 910)
+    green_button = Button(GREEN_ON, 480, 290)
     run(globals())
