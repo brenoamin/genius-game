20a71bf059c7e367f57f4dff026c8019cac5d356
Breno Amin <brenomatos125@gmail.com>
2023-06-28 22:24:18 -0300

    update: add the new_sequence animation using counters

--- src/main.py
+++ src/main.py
@@ -1,25 +1,19 @@
 from tupy import *
 from utils.buttons_mode import YELLOW_ON, GREEN_ON, RED_ON, BLUE_ON, YELLOW_OFF, GREEN_OFF, RED_OFF, BLUE_OFF, \
     BACKGROUND_SCENE, START
-from time import sleep
-from threading import Timer
 import random
 
-'''class Field(Image):
-    def __init__(self):
-        self.file = BACKGROUND_SCENE
-        self.x = 450
-        self.y = 270
-'''
 
 class Button(Image):
-    def __init__(self, file, x, y):
+    def __init__(self, file, x, y, off_file):
         self.file = file
         self.x = x
         self.y = y
-        self.toggle_count = 0
-        self.toggle_limit = 30
-       # self.pressed_button = False
+        self.off_file = off_file
+        self.is_blinking = False
+        self.blink_interval = 20  # Defina o intervalo desejado em frames
+        self.blink_counter = 0
+        self.is_button_on = False
 
     def toggle(self):
         mapping = {
@@ -36,19 +30,26 @@ class Button(Image):
         if self.file in mapping:
             self.file = mapping[self.file]
 
-    def press(self):
-        self.toggle()
-        self.toggle_count = self.toggle_limit
-        #self.pressed_button = True
+    def start_blink(self):
+        self.is_blinking = True
+        self.blink_counter = 0
+        self.is_button_on = True
 
-    def decrement_toggle_count(self):
-        if self.toggle_count > 0:
-            self.toggle_count -= 1
-            if self.toggle_count == 0:
-                self.toggle()
+    def stop_blink(self):
+        self.is_blinking = False
+        self.file = self.off_file  # Certifique-se de que o botão fique apagado após piscar
+        self.is_button_on = False
+
+    def press(self):
+        self.start_blink()
 
     def update(self):
-        self.decrement_toggle_count()
+        if self.is_blinking:
+            self.blink_counter += 1
+            if self.blink_counter >= self.blink_interval:
+                self.toggle()
+                self.blink_counter = 0
+
 
 class Game(Image):
     def __init__(self):
@@ -58,53 +59,51 @@ class Game(Image):
         self.sequence = []
         self.next_step = 0
         self.level = 5
-        self.ok = True
-        # Quando instanciamos os botões dentro da classe game eles não recebem os nomes das variáveis, logo ficam numerados no tupy
-        '''self.buttons = [
-            Button(YELLOW_OFF, 321, 396),
-            Button(RED_OFF, 572, 145),
-            Button(BLUE_OFF, 578, 395),
-            Button(GREEN_OFF, 320, 145),
-            Button(START, 447, 314)
-        ]'''
-        self.toggle_count = 0
-        self.toggle_limit = 60
+        self.blink_index = 0
+        self.blink_counter = 0
+        self.blink_interval = 30  # Defina o intervalo desejado em frames
 
     def set_buttons(self, buttons):
         self.buttons = buttons
 
     def new_sequence(self):
-        """gera uma sequência com "n" passos.
-        0 -> Amarelo
-        1 -> Vermelho
-        2 -> Azul
-        3 -> Verde
-        """
         self.sequence = [random.randint(0, 3) for _ in range(self.level)]
         self.next_step = 0
-        self.test_sequence()
-
-        for valor in self.sequence:
-            print(f"valor - {valor}")
-            Timer(10, self.buttons[valor].press()).start()
-            print(f'Botão {self.buttons[valor]} ligado')
+        self.blink_index = 0
+        self.blink_counter = 0
 
         return self.sequence
 
-    #def button_click(self, valor):
-     #   self.buttons[valor].press()
+    def blink_buttons(self):
+        if self.blink_index >= len(self.sequence):
+            # Todos os botões foram piscados, então vamos parar o blinking
+            for button in self.buttons:
+                button.stop_blink()
+            return
 
-    def test_sequence(self):
-        print(self.sequence)
+        button = self.buttons[self.sequence[self.blink_index]]
+        if self.blink_counter == 0:
+            button.start_blink()
+        elif self.blink_counter >= self.blink_interval:
+            button.stop_blink()
+
+        self.blink_counter += 1
+        if self.blink_counter >= self.blink_interval * 2:
+            self.blink_counter = 0
+            self.blink_index += 1
+
+    def update(self):
+        if self.blink_index < len(self.sequence):
+            self.blink_buttons()
 
 
 if __name__ == '__main__':
-
     game = Game()
-    yellow_button = Button(YELLOW_OFF, 321, 396)
-    red_button = Button(RED_OFF, 572, 145)
-    blue_button = Button(BLUE_OFF, 578, 395)
-    green_button = Button(GREEN_OFF, 320, 145)
+
+    yellow_button = Button(YELLOW_OFF, 321, 396, YELLOW_OFF)
+    red_button = Button(RED_OFF, 572, 145, RED_OFF)
+    blue_button = Button(BLUE_OFF, 578, 395, BLUE_OFF)
+    green_button = Button(GREEN_OFF, 320, 145, GREEN_OFF)
     game.set_buttons([yellow_button, red_button, blue_button, green_button])
 
     run(globals())
